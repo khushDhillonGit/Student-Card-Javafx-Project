e2fa279ec2220588dccd02207c146577559563b3
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2022-02-23 03:23:46 -0500

    setters and getters with correct validations

--- src/main/java/com/example/assignment1/Student.java
+++ src/main/java/com/example/assignment1/Student.java
@@ -1,12 +1,28 @@
 package com.example.assignment1;
 
+import javafx.scene.image.Image;
+
 import java.util.List;
+import java.util.Locale;
 
 public class Student {
     private String firstName;
     private String lastName;
     private int studentNumber;
     private List<String> activities;
+    private Image studentImage;
+
+    public Student(String firstName, String lastName, int studentNumber, List<String> activities) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setStudentNumber(studentNumber);
+
+        String fileName = String.format("images/pic.jpeg");
+        studentImage = new Image(getClass().getResource(fileName).toExternalForm());
+
+    }
+
+
 
     public String getFirstName() {
         return firstName;
@@ -14,7 +30,8 @@ public class Student {
 
     public void setFirstName(String firstName) {
 
-        if(firstName.matches("[A-Za-z]]") && firstName.length() >= 2){
+        if(firstName.length() >= 2){
+           firstName = firstName.substring(0,1).toUpperCase() + firstName.substring(1);
             this.firstName = firstName;
         }else{
             throw new IllegalArgumentException("Please provide a valid firstname");
@@ -28,7 +45,8 @@ public class Student {
     }
 
     public void setLastName(String lastName) {
-        if(lastName.matches("[A-Za-z]]") && lastName.length() >= 2){
+        if(lastName.length() >= 2){
+            lastName = lastName.substring(0,1).toUpperCase() + lastName.substring(1);
             this.lastName = lastName;
         }else{
             throw new IllegalArgumentException("Please provide a valid lastname");
@@ -41,7 +59,11 @@ public class Student {
     }
 
     public void setStudentNumber(int studentNumber) {
-        this.studentNumber = studentNumber;
+        if(studentNumber >= 100000000 && studentNumber <= 999999999){
+            this.studentNumber = studentNumber;
+        }else{
+            throw new IllegalArgumentException("Please provide a valid Student Number");
+        }
     }
 
     public List<String> getActivities() {
